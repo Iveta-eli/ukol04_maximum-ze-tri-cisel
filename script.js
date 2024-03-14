0d8ddfe4a8bd4b805bb91bdd0246e51d27411963
Iveta Eliášková <eliaskova.i@gmail.com>
2024-03-14 22:09:59 +0100

    ukol 04 "ukol 04 - maximum ze tří čísel"

---
+++ script.js
@@ -0,0 +1,10 @@
+const number1 = Number(prompt("čislo1"))
+const number2 = Number(prompt("čislo2"))
+const number3 = Number(prompt("čislo3"))
+
+if(number1 > number2 && number3) {
+    console.log(number1)
+} else if (number2 > number3) {
+    console.log(number2)
+} else
+    console.log(number3)
